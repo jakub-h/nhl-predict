a54cf6abd91757241abe28d2eef4a5c3f093f3c2
Jakub Hruska <jhruska@mail.muni.cz>
2020-04-07 15:48:09 +0200

    extra random tree gridsearch

--- nhl-predict.py
+++ nhl-predict.py
@@ -24,6 +24,46 @@ def mean_confidence_interval(data, confidence=0.95):
     return m, h
 
 
+def erf_grid_search_CV(dm, param_grid):
+    print("## Grid Search CV ##: Extreme Random Forest")
+    results = {'dummy_teams': [], 'dummy_coaches': [], 'scale': []}
+    for param_name in ParameterGrid(param_grid)[0].keys():
+        results[param_name] = []
+    results['acc_train'] = []
+    results['ci_train'] = []
+    results['acc_test'] = []
+    results['ci_test'] = []
+    results['time'] = []
+    for scale in [None, 'std']:
+        for params in ParameterGrid(param_grid):
+            results['dummy_teams'].append(False)
+            results['dummy_coaches'].append(False)
+            results['scale'].append(scale)
+            for param_name in params.keys():
+                results[param_name].append(params[param_name])
+            results_train = []
+            results_test = []
+            print(params)
+            print("'dummy_teams': {}, 'dummy_coaches': {}, 'scale': {} ---> ".format(False, False, scale), end='', flush=True)
+            start = time.time()
+            for fold_i in range(5):
+                x_train, x_test, y_train, y_test = dm.get_fold(fold_i, False, False, scale)
+                clf = ExtraTreesClassifier(**params, n_jobs=15)
+                clf.fit(x_train, y_train)
+                results_train.append(clf.score(x_train, y_train)*100)
+                results_test.append(clf.score(x_test, y_test)*100)
+            m_train, ci_train = mean_confidence_interval(results_train)
+            m_test, ci_test = mean_confidence_interval(results_test)
+            end = time.time()
+            results['acc_train'].append(m_train)
+            results['ci_train'].append(ci_train)
+            results['acc_test'].append(m_test)
+            results['ci_test'].append(ci_test)
+            results['time'].append(np.round(end-start, 2))
+            print("train: {:.2f}% +-{:.2f}%; test: {:.2f}% +-{:.2f}%".format(m_train, ci_train, m_test, ci_test))
+            print(25*"-", "done in {:.2f} s".format(end-start), 25*"-")
+    results = pd.DataFrame.from_dict(results)
+    results.to_csv("results/extra_trees_gridsearch.csv")
 
 
 def rf_grid_search_CV(dm, param_grid):
@@ -70,18 +110,12 @@ def rf_grid_search_CV(dm, param_grid):
 
 if __name__ == "__main__":
     dm = DatasetManager(games_hr_fn=None)
-    results_train = []
-    results_test = []
-    for i in range(5):
-        x_train, x_test, y_train, y_test = dm.get_fold(i, False, False, None)
-        clf = RandomForestClassifier(n_estimators=800, criterion='entropy', max_depth=500, min_samples_split=8,
-                                     min_samples_leaf=1, max_features=None, verbose=1, n_jobs=15)
-        clf.fit(x_train, y_train)
-        results_train.append(clf.score(x_train, y_train)*100)
-        results_test.append(clf.score(x_test, y_test)*100)
-    print(results_train)
-    print(results_train)
-    m_train, ci_train = mean_confidence_interval(results_train)
-    m_test, ci_test = mean_confidence_interval(results_test)
-    print("Train: {:.2f}% +-{:.2f}%".format(m_train, ci_train))
-    print("Test: {:.2f}% +-{:.2f}%".format(m_test, ci_test))
+    params = {
+        'criterion': ['entropy'],
+        'max_depth': [20, 40, 80, 160, 320, 640],
+        'min_samples_split': [2, 4, 8, 16],
+        'min_samples_leaf': [1, 2, 3, 4, 5, 6],
+        'max_features': [None],
+        'splitter': ['random', 'best']
+    }
+    erf_grid_search_CV(dm, params)
