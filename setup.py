d6c8a2779a65f21e90e13a3a3d4e921b4009f921
Jakub Hruska <jhruska@mail.muni.cz>
2021-04-17 17:06:24 +0200

    Create python package and folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="2.0",
+    description="NHL predict",
+    author="Jakub Hruska",
+    author_mail="jhruska@mail.muni.cz",
+    license="",
+    packages=find_packages(),
+)
