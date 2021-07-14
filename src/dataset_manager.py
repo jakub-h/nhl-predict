f838d3052b935d69347a578f18b53cc9b8abe458
Jakub Hruska <jhruska@mail.muni.cz>
2021-07-14 13:52:56 +0200

    Add calculation of in-game stats to DatasetManager

--- src/dataset_manager.py
+++ src/dataset_manager.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from pathlib import Path
 import pickle
+from src.xg_model_base import XGModel
+import matplotlib.pyplot as plt
 
 
 class DatasetManager:
     """
-    In-game stats: (*_away / *_home)
-    G, SOG, SMISS, FO%, GIV, TAKE, PP, PIM, PPG, HIT, BLK,
+    In-game stats (post game)
+    Season stats (pre game)
     """
     def __init__(self, data_path):
         if isinstance(data_path, Path):
@@ -17,11 +19,138 @@ class DatasetManager:
             raise ValueError(f"'data_path' must be a string or instance of Path class. Not {type(data_path)}.")
 
     def calculate_in_game_stats(self, season):
+        """
+        - away_ / home_
+        - _EV, _PP, _SH
+        - G, SOG, SMISS, HIT, TAKE, GIVE, FOW, PPO, PIM, BLK, CORSI
+        - xG
+        :param season:
+        :return:
+        """
         # Load games raw play-by-play
         with open(self._data_path / "games_raw" / f"{season}-{season+1}.pickle", 'rb') as f:
             games_raw = pickle.load(f)
+
         # Load xG play-by-play
         xg_pbp = pd.read_csv(self._data_path / "xg_pbp" / f"{season}-{season+1}.csv", index_col=0)
-        print(xg_pbp.info())
 
-        # return games_raw
+        # Load xG model
+        xg_model = XGModel(self._data_path, model="../models/xg_extra_trees_n10")
+
+        # Calculate xG predictions for all events (all games in the season)
+        xg_pred = xg_model.predict(xg_pbp)
+        xg_pbp['xg_pred'] = xg_pred
+
+        # Define stats scheme
+        stat_names = ["G", "SOG", "SMISS", "HIT", "TAKE", "GIVE", "FOW", "BLK", "CORSI", "xG"]
+        schema = dict()
+        for team in ['away', 'home']:
+            for situation in ['ALL', 'EV', 'PP', 'SH']:
+                for stat in stat_names:
+                    schema[f"{team}_{stat}_{situation}"] = 0
+            schema[f"{team}_PPO"] = 0
+
+        # Calculate the stats
+        games = []
+        for raw_game in games_raw:
+            game_stats = schema.copy()
+            for play in raw_game['plays']:
+                self._process_play(play, raw_game['teams'], game_stats)
+            self._assign_xg_to_game(game_stats, raw_game['id'], xg_model)
+
+            # Calculate CORSI
+            for team in ['away', 'home']:
+                for situation in ['ALL', 'EV', 'PP', 'SH']:
+                    game_stats[f"{team}_CORSI_{situation}"] = game_stats[f"{team}_G_{situation}"] +\
+                                                              game_stats[f"{team}_SOG_{situation}"] +\
+                                                              game_stats[f"{team}_SMISS_{situation}"]
+
+                    # Add blocked shots (BLK is number of blocks of opposing team)
+                    if team == "away":
+                        game_stats[f"away_CORSI_{situation}"] += game_stats[f"home_BLK_{situation}"]
+                    else:
+                        game_stats[f"home_CORSI_{situation}"] += game_stats[f"away_BLK_{situation}"]
+
+            for team in ['away', 'home']:
+                for situation in ['ALL', 'EV', 'PP', 'SH']:
+                    # Filter subset of events
+                    home_flag = 1 if team == "home" else 0
+                    subset = xg_pbp[(xg_pbp['game_id'] == raw_game['id']) & (xg_pbp['is_home'] == home_flag)]
+                    if situation == "EV":
+                        subset = subset[subset['strength_active'] == subset['strength_opp']]
+                    elif situation == "PP":
+                        subset = subset[subset['strength_active'] > subset['strength_opp']]
+                    elif situation == "SH":
+                        subset = subset[subset['strength_active'] < subset['strength_opp']]
+
+                    # Calculate sum of xGs for events
+                    xg_sum = 0 if subset.empty else subset['xg_pred'].sum()
+                    game_stats[f'{team}_xG_{situation}'] = xg_sum
+
+            games.append(game_stats)
+        df = pd.DataFrame(games)
+        df.index += 1
+        return pd.DataFrame(games)
+
+    @staticmethod
+    def _process_play(play, teams, game_stats):
+        """
+        Calculate post-game statistics from plays
+        :param play:
+        :param game_stats:
+        :return:
+        """
+        # Determine active team
+        if play['team']['id'] == teams['away']['id']:
+            team = "away"
+        else:
+            team = "home"
+
+        # Determine type of the stat
+        if play['type'] == "GOAL":
+            stat = "G"
+        elif play['type'] == "SHOT":
+            stat = "SOG"
+        elif play['type'] == "MISSED_SHOT":
+            stat = "SMISS"
+        elif play['type'] == "HIT":
+            stat = "HIT"
+        elif play['type'] == "TAKEAWAY":
+            stat = "TAKE"
+        elif play['type'] == "GIVEAWAY":
+            stat = "GIVE"
+        elif play['type'] == "BLOCKED_SHOT":
+            stat = "BLK"
+        elif play['type'] == "FACEOFF":
+            stat = "FOW"
+        elif play['type'] == "PENALTY":
+            stat = "PPO"
+            team = "away" if team == "home" else "home"     # switch team (penalty on vs. powerplay opportunity)
+        else:
+            raise ValueError(f"W: Unknown type of play: {play['type']}")
+
+        # Determine strength situation (even, pp, sh)
+        if play['strength_active'] == play['strength_opp']:
+            situation = "EV"
+        elif play['strength_active'] > play['strength_opp']:
+            situation = "PP"
+        else:
+            situation = "SH"
+
+        # Increase the stat
+        if stat != "PPO":
+            game_stats[f"{team}_{stat}_{situation}"] += 1
+            game_stats[f"{team}_{stat}_ALL"] += 1
+        else:
+            game_stats[f"{team}_PPO"] += 1
+
+    def _assign_xg_to_game(self, game_stats, game_id, xg_model):
+        """
+        Calculate xG score of whole game (per team)
+        :param game_stats:
+        :param game_id:
+        :param xg_model:
+        :return:
+        """
+        # TODO
+        pass
--- src/xg_model_base.py
+++ src/xg_model_base.py
@@ -15,6 +15,7 @@ class XGModel:
     Model for predicting xG (expected goals) for each unblocked shot. This base class takes a Scikit-learn
     model (as a parameter in constructor) and works as a wrapper for this model.
     """
+
     def __init__(self, data_path, model, random_state=37):
         self._random_state = random_state
 
@@ -28,11 +29,16 @@ class XGModel:
 
         # Load model if already fitted and stored
         if isinstance(model, str) or isinstance(model, Path):
-            with open(model, "rb") as f:
+            with open(f"{model}.pkl", "rb") as f:
                 self._model = pickle.load(f)
-        # Initiate fresh unfitted model
+            with open(f"{model}_out-scaler.pkl", 'rb') as f:
+                self._output_scaler = pickle.load(f)
         else:
+            # Initiate fresh unfitted model
             self._model = model
+            # Initiate fresh unfitted output scaler (Optimized for ExtraTreeRegressor!)
+            self._output_scaler = Pipeline([('quantile', QuantileTransformer(output_distribution="normal")),
+                                            ('minmax', MinMaxScaler())])
 
         # Load datasets and split into train/test
         self.x_train, self.x_test, self.y_train, self.y_test = self._get_datasets()
@@ -40,10 +46,6 @@ class XGModel:
         # Fit data scaler
         self._scaler = StandardScaler()
         self._scaler.fit(self.x_train.drop(columns=['game_id']))
-
-        # Init output scaler (Optimized for ExtraTreeRegressor!)
-        self._output_scaler = Pipeline([('quantile', QuantileTransformer(output_distribution="normal")),
-                                        ('minmax', MinMaxScaler())])
 
     def _get_datasets(self):
         """
@@ -67,8 +69,8 @@ class XGModel:
         df.reset_index(drop=True, inplace=True)
 
         # Create dummy variables from categorical
-        df = pd.get_dummies(df, prefix_sep="-", columns=['shot_type', 'prev_event_type'])\
-               .drop(columns=['shot_type-None'])
+        df = pd.get_dummies(df, prefix_sep="-", columns=['shot_type', 'prev_event_type']) \
+            .drop(columns=['shot_type-None'])
 
         return train_test_split(df.drop(columns=['outcome']), df['outcome'],
                                 random_state=self._random_state, train_size=.8)
@@ -115,12 +117,32 @@ class XGModel:
 
     def predict(self, x):
         """
-        Predicts classes for given shot-dataset.
+        Predicts xG (probabilities) for given shot-dataset.
 
         :param x:
         :return: y_pred - predicted classes (goal vs. no-goal)
         """
-        x_scaled = self.scale_dataset(x)
+        df = x.copy()
+        # Clean data
+        if "shot_type" in df.columns:
+            # Clean data
+            df.fillna("None", inplace=True)
+            df['shot_type'] = df['shot_type'].apply(lambda s: s.split(" ")[0])
+            df = df.fillna(0)\
+                   .reset_index(drop=True)
+
+            # Create dummy variables from categorical
+            df = pd.get_dummies(df, prefix_sep="-", columns=['shot_type', 'prev_event_type'])
+            if 'shot_type-None' in df.columns:
+                df.drop(columns=['shot_type-None'], inplace=True)
+            if 'outcome' in df.columns:
+                df.drop(columns=['outcome'], inplace=True)
+            # Correct the columns
+            for col in self.x_train.columns:
+                if col not in df.columns:
+                    df[col] = 0
+
+        x_scaled = self.scale_dataset(df)
         y = self._model.predict(x_scaled).reshape(-1, 1)
         return pd.DataFrame(self._output_scaler.transform(y), columns=['xG'])['xG']
 
@@ -140,5 +162,7 @@ class XGModel:
     '''
 
     def save(self, filename):
-        with open(filename, "wb") as f:
+        with open(f"{filename}.pkl", "wb") as f:
             pickle.dump(self._model, f)
+        with open(f"{filename}_out-scaler.pkl", "wb") as f:
+            pickle.dump(self._output_scaler, f)
